0da24fb5d3c50793c3239107a8c3165857f64f84
majidied <majidimajidi2003@gmail.com>
2024-08-07 10:17:58 +0100

    Refactor DBClient class to include methods for checking the client's status and retrieving the number of users and files in the database

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,8 @@
 import { MongoClient } from 'mongodb';
 
+/**
+ * Represents a database client for interacting with MongoDB.
+ */
 class DBClient {
   constructor() {
     const url = `mongodb://${process.env.DB_HOST || 'localhost'}:${process.env.DB_PORT || 27017}/`;
@@ -14,14 +17,26 @@ class DBClient {
       });
   }
 
+  /**
+     * Checks if the database client is alive.
+     * @returns {boolean} True if the client is alive, false otherwise.
+     */
   isAlive() {
     return !!this.client;
   }
 
+  /**
+     * Retrieves the number of users in the database.
+     * @returns {Promise<number>} A promise that resolves to the number of users.
+     */
   async nbUsers() {
     return this.db.collection('users').countDocuments();
   }
 
+  /**
+     * Retrieves the number of files in the database.
+     * @returns {Promise<number>} A promise that resolves to the number of files.
+     */
   async nbFiles() {
     return this.db.collection('files').countDocuments();
   }
